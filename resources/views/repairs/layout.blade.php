8bb1ac4be77911339bc6b77bfbc033371d289909
Marc <marcsanuy@gmail.com>
2019-05-22 18:50:04 +0200

    new update

--- resources/views/repairs/layout.blade.php
+++ resources/views/repairs/layout.blade.php
@@ -6,21 +6,33 @@
 
     <title>CARSTORY - GARAJE</title>
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css"
+        rel="stylesheet">
 
+    <style>
+        body {
+            background-color: #CACACA;
+            color: black;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+    </style>
 </head>
 
 <body>
 
-  
 
-<div class="container">
 
-    @yield('content')
+    <div class="container">
 
-</div>
+        @yield('content')
 
-   
+    </div>
+
+
 
 </body>
 
